4b7eeeb4c68c6f60139911c90441b525d1017d72
GersonHernandezGaravito <ghernandezg13@miumg.edu.gt>
2021-05-14 16:44:00 -0600

    se crea interfaz de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+//import {LoginPage} from './auth/login/login.page'
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./auth/login/login.module').then( m => m.LoginPageModule)
+    //component: LoginPage
+  },  {
+    path: 'signup',
+    loadChildren: () => import('./auth/signup/signup.module').then( m => m.SignupPageModule)
   }
+
+
 ];
 @NgModule({
   imports: [
