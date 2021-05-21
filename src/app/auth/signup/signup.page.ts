80c3ae360f4a5b68a033cfe91869126711f248a1
GersonHernandezGaravito <ghernandezg13@miumg.edu.gt>
2021-05-21 05:12:53 -0600

    creación de signup

--- src/app/auth/signup/signup.page.ts
+++ src/app/auth/signup/signup.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from  "@angular/router";
+import { AuthService } from '../../service/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
-
-  constructor() { }
+  usuario = {
+    nombreU: '',
+    correo: '',
+    contrasena: '',
+    intentos: '10'
+  }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
   }
 
+  register(){
+    
+    this.authService.signup(this.usuario)
+    .subscribe(
+      res => {
+        console.log(res)
+        //Se puede guardar en Cookies
+        localStorage.setItem('token', res.token);
+        localStorage.setItem('rol', res.rol);
+        
+        this.router.navigate(['/tabs'])
+      },
+      err => console.log(err)
+    )
+  }
 }
